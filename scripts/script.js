9047ad1101fe76bcd41283126945ee26e722c3e9
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-13 17:45:42 +0200

    - Add game function to loop 5 times rounds and prompt the user for selection - fix a logical error, where the case for Player scissors and Computer rock was resulting in a tie. - Return result of game as a list of string and a number for better result calculation

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,8 @@ function computerPlay() {
 // console.log(computerPlay())
 
 function playRound(playerSelection, computerSelection) {
-    console.log('player:',playerSelection, 'computer', computerSelection);
-    
+    console.log('player:',playerSelection, 'computer:', computerSelection);
+
     const table = {
         'rock': ''+1,
         'paper': ''+2,
@@ -25,24 +25,38 @@ function playRound(playerSelection, computerSelection) {
         case '11':
         case '22':
         case '33':
-             result = `It's a tie`;            
+             result = [`It's a tie`, 0];
             break;
         case '13':
         case '32':
         case '21':
-            result = `You win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`
+            result = [`You win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`, 1]
             break;
         case '12':
         case '23':
-            result = `You lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
+        case '31':
+            result = [`You lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`, -1];
             break;
         default:
-            result = 'Error';
+            result = ['Error', 0];
             break;
     }
     return result
 }
 
-let playerSelection = 'rock'
-let computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+// let playerSelection = 'rock'
+// let computerSelection = computerPlay()
+// console.log(playRound(playerSelection, computerSelection))
+
+function game() {
+    let result = 0
+    let playerSelection;
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt(`rock, paper or scissors?`, 'rock')
+        result += playRound(playerSelection, computerPlay())[1]
+    }
+    console.log(result);
+    return result > 0 ? 'Player Wins' : result < 0 ? 'Computer wins' : 'It\'s a tie'
+}
+
+console.log(game());
